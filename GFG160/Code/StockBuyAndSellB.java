d6a6fda5165b61d8439137df20baedbc422d78be
DenaLa <90577531+DenaLa@users.noreply.github.com>
2025-05-07 18:16:12 -0400

    Create StockBuyAndSellB.java
    Add problem 08 code

---
+++ GFG160/Code/StockBuyAndSellB.java
@@ -0,0 +1,14 @@
+class StockBuyAndSellB {
+    public int maximumProfit(int prices[]) {
+        // Code here
+        int min = prices[0];
+        int maxProfit = 0;
+
+        for(int i = 1; i < prices.length; i++){
+            min = Math.min(min, prices[i]);
+            
+            maxProfit = Math.max(maxProfit, prices[i]-min);
+        }
+            return maxProfit;
+    }
+}
